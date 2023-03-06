af26411eb5fb60a9debcfda8dadee1812b3a205a
Rodrigo Guimarães <rguimaraes@ons.org.br>
2023-03-06 20:24:34 -0300

    botoes adicionar e deletar (tela de tarefas)

--- app/src/main/java/br/com/biexpert/bicm/MainActivity.kt
+++ app/src/main/java/br/com/biexpert/bicm/MainActivity.kt
@@ -16,30 +16,62 @@ class MainActivity : AppCompatActivity() {
         //endregion
 
         //region btn sair
-        findViewById<View>(R.id.logout).setOnClickListener{
+        findViewById<View>(R.id.logout).setOnClickListener {
             val activity = Intent(this, LoginScreenActivity::class.java);
             startActivity(activity);
         }
         //endregion
 
         //region listView
-        var listViewTasks = findViewById<android.widget.ListView>(R.id.listViewTasks);
-        var createTask = findViewById<android.widget.EditText>(R.id.createTask);
+        val listViewTasks = findViewById<android.widget.ListView>(R.id.listViewTasks);
+        val createTask = findViewById<android.widget.EditText>(R.id.createTask);
 
-        var itemList = arrayListOf<String>();
-        var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, itemList);
+        val itemList = arrayListOf<String>();
+        val adapter =
+            ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, itemList);
 
+
+        //region btnAdd
         findViewById<View>(R.id.add).setOnClickListener {
             itemList.add(createTask.text.toString());
             listViewTasks.adapter = adapter;
             //faz o refresh da tela
             adapter.notifyDataSetChanged()
-
             createTask.text.clear()
-        }
 
+        }
         //endregion
 
+        //region btnClear
+        findViewById<View>(R.id.clear).setOnClickListener {
+            itemList.clear()
+            adapter.notifyDataSetChanged()
+
+        }
+        //endregion
+
+
+        //region btnDeletar
+//        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+        findViewById<View>(R.id.delete).setOnClickListener{
+
+            val selectedItems = listViewTasks.checkedItemPositions
+            for (i in selectedItems.size() - 1 downTo 0) {
+                if (selectedItems.valueAt(i)) {
+                    val selectedItem = adapter.getItem(selectedItems.keyAt(i)) as String
+                    adapter.remove(selectedItem)
+                }
+            }
+            listViewTasks.clearChoices()
+
+
+        }
+        //endregion
+
+
+
+
+        //endregion
 
 
     }
