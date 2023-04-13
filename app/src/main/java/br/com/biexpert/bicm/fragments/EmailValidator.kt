e025e0d27f307bb0b427708b74fa689bf8cff68c
Rodrigo Guimarães <rguimaraes@ons.org.br>
2023-04-12 22:55:58 -0300

    ajuste bugs no validador de e-mail

--- app/src/main/java/br/com/biexpert/bicm/fragments/EmailValidator.kt
+++ app/src/main/java/br/com/biexpert/bicm/fragments/EmailValidator.kt
@@ -36,8 +36,6 @@ class EmailValidator : Fragment() {
 
         textEmailAddress = view.findViewById(R.id.email_input) as EditText
 
-
-
         textEmailAddress.addTextChangedListener(object : TextWatcher {
 
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
